6fefc86d5d11f35e75422357a8d66ed289270bfa
Rishab Narang <narangrishab@gmail.com>
2017-03-09 22:49:46 +0530

    Feature: completed week2 Need to review week2 and week 3 Starting week3 assignment

--- Week3/ex2./costFunction.m
+++ Week3/ex2./costFunction.m
@@ -7,7 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -21,7 +21,9 @@ grad = zeros(size(theta));
 %
 
 
-
+h = sigmoid(X * theta);
+J = (1/m) * (-y' * log(h) - (1-y)' * log(1-h));
+grad = (1/m) * X' * (h-y);
 
 
 
