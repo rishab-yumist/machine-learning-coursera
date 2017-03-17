a0c6105637d608c98a942dc4f752da692799c1ed
Rishab Narang <narangrishab@gmail.com>
2017-03-17 15:11:08 +0530

    Feature: Week4 code added Submitted the code Scored 100/100

--- Week3/ex2/costFunction.m
+++ Week3/ex2/costFunction.m
@@ -9,8 +9,8 @@ m = length(y); % number of training examples
 
 % You need to return the following variables correctly
 J = 0;
-grad = zeros(size(theta));
 
+grad = zeros(size(theta));
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
 %               You should set J to the cost.
@@ -19,9 +19,8 @@ grad = zeros(size(theta));
 %
 % Note: grad should have the same dimensions as theta
 %
-
 h = sigmoid(X * theta);
-J = (1/m) * (-y' * log(h) - (1-y)' * log(1-h));
+J = (1/m) * (-y' * log(h) - (1-y)' * log(1-h))
 grad = (1/m) * X' * (h-y);
 
 
